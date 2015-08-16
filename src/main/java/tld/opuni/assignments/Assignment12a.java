64d821e662855a20d9af38c856cf0e8989ff3fef
wvxvw <olegsivokon@gmail.com>
2015-08-16 21:30:31 +0300

    First part of second assignment finished.

--- src/main/java/tld/opuni/assignments/Assignment12a.java
+++ src/main/java/tld/opuni/assignments/Assignment12a.java
@@ -10,7 +10,13 @@ import java.util.Scanner;
 import java.util.Locale;
 import java.util.InputMismatchException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Assignment12a extends Assignment {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(Assignment12a.class);
 
     private class Pair extends AbstractMap.SimpleEntry<Integer,Double> {
         
@@ -26,6 +32,8 @@ public class Assignment12a extends Assignment {
         private final int workday;
 
         private final int year;
+
+        private final int hourlyRate = 35;
 
         public January() {
             this(Calendar.getInstance().get(Calendar.YEAR), 2.0d, 8);
@@ -51,6 +59,46 @@ public class Assignment12a extends Assignment {
         }
 
         public int getYear() { return year; }
+
+        public int getWorkday() { return workday; }
+
+        public double getOvertime() { return overtime; }
+
+        public Pair max() {
+            Pair result = new Pair(-1, Double.MIN_VALUE);
+            for (Pair day : this)
+                if (result.getValue() < day.getValue()) result = day;
+            return result;
+        }
+
+        public int daysWorked() {
+            int result = 0;
+            for (Pair day : this) if (day.getValue() > 0) result += 1;
+            return result;
+        }
+
+        public double hoursWorked() {
+            double result = 0;
+            for (Pair day : this) result += hoursPerDay(day);
+            return result;
+        }
+
+        public double computePay(final Pair day) {
+            return hoursPerDay(day) * hourlyRate;
+        }
+
+        public double paycheck() {
+            double result = 0d;
+            for (Pair day : this)
+                if (day.getValue() > 0) result += computePay(day);
+            return result;
+        }
+
+        private double hoursPerDay(final Pair day) {
+            final double hours = day.getValue();
+            return Math.min(hours, workday) +
+                    Math.max(hours - workday, 0) * overtime;
+        }
         
         private LinkedList<Pair> calculateWorkdays() {
             final LinkedList<Pair> result = new LinkedList<Pair>();
@@ -81,16 +129,27 @@ public class Assignment12a extends Assignment {
         
         for (Pair day : january) {
             cld.set(january.getYear(), 0, day.getKey());
-            String wday = cld.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.US);
+            String wday = cld.getDisplayName(
+                Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.US);
             int date = day.getKey();
             System.out.format("How much did you work on %s %d?%n", wday, date + 1);
             try {
-                double hours = scanner.nextDouble();
+                day.setValue(scanner.nextDouble());
             } catch (InputMismatchException e) {
-                System.out.println("Was reading: " + scanner.next());
+                logger.debug("Was reading: " + scanner.next());
                 break;
             }
         }
+        final Pair max = january.max();
+        cld.set(january.getYear(), 0, max.getKey());
+        System.out.format("The longest day of your career so far was: %s %s%n" +
+                          "On that day you earned %.2f shekels.%n" +
+                          "You have worked for %d days, during which you clocked %.2f hours%n" +
+                          "which earned you %.2f shekels.%n",
+                          cld.getDisplayName(Calendar.DAY_OF_WEEK,
+                                             Calendar.SHORT, Locale.US),
+                          max.getKey() + 1, january.computePay(max),
+                          january.daysWorked(), january.hoursWorked(), january.paycheck());
     }
 
     private January readJanuary(final Scanner scanner) {
@@ -104,12 +163,15 @@ public class Assignment12a extends Assignment {
                     System.out.println("How long is your working day?");
                     january = new January(year, overtime, scanner.nextInt());
                 } catch (InputMismatchException e) {
+                    scanner.next();
                     january = new January(year, overtime);
                 }
             } catch (InputMismatchException e) {
+                scanner.next();
                 january = new January(year);
             }
         } catch (InputMismatchException e) {
+            scanner.next();
             january = new January();
         }
         return january;
