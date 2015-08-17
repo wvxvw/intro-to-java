7e12949707654a2f7fef16d53d23f4456d258bce
wvxvw <olegsivokon@gmail.com>
2015-08-17 21:30:37 +0300

    Fixed first part of third assignment.

--- src/main/java/tld/opuni/assignments/Assignment13b.java
+++ src/main/java/tld/opuni/assignments/Assignment13b.java
@@ -2,6 +2,7 @@ package tld.opuni.assignments;
 
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.LinkedList;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,22 +29,19 @@ public class Assignment13b extends Assignment {
      * @param matrix An integer matrix.
      */
     public static boolean isUpperOddsLowerEvens(final int[][] matrix) {
-        boolean result = true;
-        outer: for (int rows = 0; rows < matrix.length; rows++) {
-            for (int columns = rows + 1;
-                 columns < matrix[rows].length; columns++) {
-                int cellxy = matrix[rows][columns];
-                int cellyx = matrix[columns][rows];
-                logger.debug("cellxy: " + cellxy + " " + rows + " " + columns);
-                logger.debug("cellyx: " + cellyx + " " + columns + " " + rows);
-                if ((cellxy != 1 && (cellxy & 1) == 1) ||
-                    (cellyx != 1 && (cellyx & 1) == 0)) {
-                    result = false;
-                    break outer;
-                }
+        assert matrix.length == matrix[0].length;
+        
+        LinkedList<Integer> topRight = new LinkedList<Integer>();
+        LinkedList<Integer> bottomLeft = new LinkedList<Integer>();
+        
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = row + 1; col < matrix.length; col++) {
+                topRight.add(matrix[row][col]);
+                bottomLeft.add(matrix[col][row]);
             }
         }
-        return result;
+        return topRight.stream().anyMatch(n -> (n & 1) == 0) &&
+                bottomLeft.stream().allMatch(n -> (n & 1) == 1);
     }
     
     /**
